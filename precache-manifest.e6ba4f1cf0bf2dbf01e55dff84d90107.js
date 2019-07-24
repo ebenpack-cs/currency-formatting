62e24d74083a2fb70c827965bb467565a5e88632
Eben Packwood <epackwood@cashstar.com>
2019-07-24 11:34:38 -0400

    add

---
+++ precache-manifest.e6ba4f1cf0bf2dbf01e55dff84d90107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0391efeae391c6582b1f349966481f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba75a4559c59fe904c5",
+    "url": "/static/css/main.0d92d693.chunk.css"
+  },
+  {
+    "revision": "7db6446fc5a5fd317934",
+    "url": "/static/js/2.4c64100d.chunk.js"
+  },
+  {
+    "revision": "aba75a4559c59fe904c5",
+    "url": "/static/js/main.1b9b0f31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
